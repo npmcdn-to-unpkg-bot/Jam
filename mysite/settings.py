3877785881ec165ee268fe4c4702c4981fa1c0e2
Evan Carter <evanrc@utexas.edu>
2016-04-28 13:43:10 -0400

    Updated STATICFILES_DIRS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = '/jam/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    '/jam/misc', 
-    )
+STATICFILES_DIRS = ( 
+      '/jam/misc/',  
+)
